f4c5b64ae6ecc57ed02de97a393bb654bb4cb574
nadyamaharanii <127836850+nadyamaharanii@users.noreply.github.com>
2023-03-25 09:25:58 +0800

    upload

---
+++ sesi2/latihan01.php
@@ -0,0 +1,19 @@
+<?php
+    $judulpage = "latihan 01 PHP"; 
+    $konten = '<h3>Latihan 01 PHP</h3><p>praktikum pemrograman web bentuk dasar PHP</p>';
+?></php><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?=$judulpage; ?> </title>
+</head>
+<body>
+    
+    <?php
+        echo $konten;
+    ?> 
+
+</body>
+</html>
---
+++ sesi2/latihan02.php
@@ -0,0 +1,12 @@
+<?php 
+    $angka=3;
+
+    echo "Nilai Angka: <strong>".$angka."</strong>";
+
+    if($angka>4){
+        //kondisi true
+        echo "<br>Nilai Angka Lebih dari 4";
+    }else{
+        //kondisi false
+        echo "<br>Nilai Angka Kurang dari sama dengan 4";
+    }
